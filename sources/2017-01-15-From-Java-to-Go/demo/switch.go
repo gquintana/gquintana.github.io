3ad43d41964cbbeea6821c4a5c3e024372026fa6
Gérald Quintana <gerald.quintana@zenika.com>
2017-01-21 11:09:12 +0100

    Golang: fix typos and add object parapgraph

--- sources/2017-01-15-From-Java-to-Go/demo/switch.go
+++ sources/2017-01-15-From-Java-to-Go/demo/switch.go
@@ -4,6 +4,21 @@ import (
 	"fmt"
 )
 
+func patternMatch(i int) {
+	// tag::pattern[]
+	switch {
+	case i == 0:
+		fmt.Print("None")
+	case i == 1:
+		fmt.Print("One ")
+	case i > 1:
+		fmt.Print("Many ")
+	default:
+		fmt.Print("Invalid")
+	}
+	// end::pattern[]
+}
+
 func main() {
 	i := 1
 	// tag::main[]
