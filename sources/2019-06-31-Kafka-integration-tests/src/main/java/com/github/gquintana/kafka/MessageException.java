da08c8b9b14da0836a17ec87c5f498dac98fad2f
Gérald Quintana <gerald.quintana@gmail.com>
2019-06-10 18:40:36 +0200

    Kafka integration tests sources

---
+++ sources/2019-06-31-Kafka-integration-tests/src/main/java/com/github/gquintana/kafka/MessageException.java
@@ -0,0 +1,11 @@
+package com.github.gquintana.kafka;
+
+public class MessageException extends RuntimeException {
+    public MessageException(String message) {
+        super(message);
+    }
+
+    public MessageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
