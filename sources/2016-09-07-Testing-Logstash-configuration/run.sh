aeff133881e68e9d7c5d319244c6b2f7f5011c26
Gérald Quintana <gerald.quintana@gmail.com>
2019-09-17 21:24:27 +0200

    Built from 93380fc

--- sources/2016-09-07-Testing-Logstash-configuration/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#LS_HOME=
-$LS_HOME/bin/logstash -f 'conf/*.conf'
--- sources/2017-01-15-From-Java-to-Go/demo/array_slice.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	a := [4]string{"One", "Two", "Three"}
-	a[0] = "First"
-	fmt.Println(a)
-}
