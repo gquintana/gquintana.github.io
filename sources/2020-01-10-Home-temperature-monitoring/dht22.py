ecb1ec394d5be37e36c1e2607ebb51d90f6bb460
Gérald Quintana <gerald.quintana@gmail.com>
2020-01-08 22:18:19 +0100

    Fix home temp monitoring post

--- sources/2020-01-10-Home-temperature-monitoring/dht22.py
+++ sources/2020-01-10-Home-temperature-monitoring/dht22.py
@@ -1,10 +1,11 @@
 import Adafruit_DHT
-from time import sleep
+from time import sleep, time
 from datetime import datetime
 from influxdb import InfluxDBClient
 import logging
 import logging.config
 import yaml
+import traceback
 
 
 def read_config():
@@ -14,23 +15,30 @@ def read_config():
 
 class DHT22Reader:
     def __init__(self, pin):
+        self.sensor = Adafruit_DHT.DHT22
         self.pin = pin
         self.logger = logging.getLogger('DHT22Reader')
         self.logger.info('Using DHT22 on pin {}'.format(str(pin)))
 
     def read(self):
         try:
-            humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22,
-                                                            self.pin)
-            if humidity and temperature:
-                self.logger.info("Temp={0:0.1f}*C Humidity={1:0.1f}%"
-                                 .format(temperature, humidity))
+            humidity, temperature = Adafruit_DHT.read_retry(self.sensor, self.pin)
+            if humidity and (humidity < 0 or humidity > 100):
+                self.logger.warn("Invalid Humidity={0:0.1f}%".format(humidity))
+                humidity = None
+
+            if temperature and (temperature < -50 or temperature > 100):
+                self.logger.warn("Invalid Temp={0:0.1f}%".format(temperature))
+                temperature = None
+
+            if humidity or temperature:
+                self.logger.info("Temp={0:0.1f}*C Humidity={1:0.1f}%".format(temperature, humidity))
                 return (self.pin, humidity, temperature)
             else:
                 self.logger.info("No data")
                 return None
         except RuntimeError as e:
-            self.logger.warn("RuntimeError: " + str(e))
+            self.logger.error("RuntimeError:" + str(e) + traceback.format_exc())
             return None
 
 
@@ -43,11 +51,11 @@ class InfluxDBWriter:
     def write(self, pin, humidity, temperature):
         time = datetime.utcnow().isoformat()
         influxdb_point = {"measurement": "dht22",
-                          "tags": {"pin": pin,
-                                   "host": "raspberrypi"},
+                          "tags": {"pin": pin, "host": "raspberrypi"},
                           "time": time,
-                          "fields": {"humidity": humidity,
-                                     "temperature": temperature}}
+                          "fields": {
+                            "humidity": humidity,
+                            "temperature": temperature}}
         self.client.write_points([influxdb_point])
 
     def close(self):
@@ -56,21 +64,30 @@ class InfluxDBWriter:
 
 def main():
     config = read_config()
-    print(str(config))
     logging.config.dictConfig(config['logging'])
+    logging.info("DHT22 starting")
     app_config = config['application']
+    # DHT22 sensor connected to GPIO12.
     reader = DHT22Reader(int(app_config['pin']))
     writer = InfluxDBWriter(app_config['database'])
     period = int(app_config['period'])
 
     try:
         while True:
+            start_time = time()
             data = reader.read()
             if data:
                 pin, humidity, temperature = data
                 writer.write(pin, humidity, temperature)
-            sleep(period)
-    except KeyboardInterrupt:
+            end_time = time()
+            duration = end_time - start_time
+            if (duration > period):
+                logging.warn("Took {}s to read/write".format(duration))
+            else:
+                sleep(period - duration)
+    except Exception as e:
+        logging.error("Exception:" + str(e) + traceback.format_exc())
+    finally:
         writer.close()
 
 
